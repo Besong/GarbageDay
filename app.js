923b61466ea84ea987ae1ea3eebbc263f1696fef
Ernest <besonganong_ernest@yahoo.com>
2026-04-14 23:40:31 -0230

    project setup

---
+++ app.js
@@ -0,0 +1,19 @@
+const path = require("node:path");
+const express = require("express");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const assetsPath = path.join(__dirname, "public")
+app.use(express.static(assetsPath))
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = 3000;
+app.listen(PORT, (error) => {
+    if (error) {
+        throw error
+    }
+    console.log(`GarbageDay app - listening on port ${PORT}!`)
+});
